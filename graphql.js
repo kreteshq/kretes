dbc1b73b1bddc1300f88c368d6924653f6fb4d31
Zaiste <oh@zaiste.net>
2018-03-24 00:45:00 +0100

    feature: graphql | move GraphQL functions into a module

---
+++ graphql.js
@@ -0,0 +1,51 @@
+// Copyright 2018 Zaiste & contributors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+const debug = require('debug')('huncwot:graphql');
+
+const Promise = require('bluebird');
+const fs = Promise.promisifyAll(require('fs-extra'));
+const path = require('path');
+const assert = require('assert');
+const mime = require('mime-types');
+
+const { runHttpQuery } = require('apollo-server-core');
+const { resolveGraphiQLString } = require('apollo-server-module-graphiql');
+const { makeExecutableSchema } = require('graphql-tools');
+
+const { ok, html } = require('./response');
+
+function graphql(options) {
+  return async request => {
+    let method = request.request.method;
+    let query = request.params;
+    let response = await runHttpQuery([], { method, options, query });
+
+    return ok(response);
+  };
+}
+
+function graphiql(options) {
+  return async request => {
+    let query = request.params;
+    let response = await resolveGraphiQLString(query, options, request);
+
+    return html(response);
+  };
+}
+
+module.exports = {
+  graphiql,
+  graphql,
+  makeSchema: makeExecutableSchema
+};
