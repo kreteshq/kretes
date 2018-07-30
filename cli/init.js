58bc4914b0dd3209882f08e26259182a0f9179fa
Zaiste <oh@zaiste.net>
2018-07-30 19:46:55 +0200

    task: update template dependencies

--- cli/init.js
+++ cli/init.js
@@ -29,10 +29,14 @@ async function init({ dir, dbengine }) {
 
     // dynamic files
     const databaseConfig = join(cwd, dir, 'config', 'database.json');
-    await fs.outputJson(databaseConfig, generateDatabaseConfig(name), { spaces: 2 });
+    await fs.outputJson(databaseConfig, generateDatabaseConfig(name), {
+      spaces: 2
+    });
 
     const packageJSON = join(cwd, dir, 'package.json');
-    await fs.outputJson(packageJSON, generatePackageJSON(dir, dbengine), { spaces: 2 });
+    await fs.outputJson(packageJSON, generatePackageJSON(dir, dbengine), {
+      spaces: 2
+    });
 
     const sql = join(cwd, dir, 'db', 'tasks.sql');
     await fs.outputFile(sql, generateSQL(name, dbengine));
@@ -49,7 +53,9 @@ async function init({ dir, dbengine }) {
     if (isYarnInstalled) {
       spawn('yarn', { cwd: dir, stdio: 'inherit' });
     } else {
-      console.error('\nError: `yarn` is not installed. Please check their installation guide at https://yarnpkg.com/en/docs/install to learn how to install `yarn` on your platform');
+      console.error(
+        '\nError: `yarn` is not installed. Please check their installation guide at https://yarnpkg.com/en/docs/install to learn how to install `yarn` on your platform'
+      );
     }
   } catch (error) {
     console.log('error: ' + error.message);
@@ -57,7 +63,9 @@ async function init({ dir, dbengine }) {
 }
 
 async function hasbin(name) {
-  return await Promise.resolve(process.env.PATH.split(delimiter).map(_ => join(_, name)))
+  return await Promise.resolve(
+    process.env.PATH.split(delimiter).map(_ => join(_, name))
+  )
     .map(exists)
     .reduce((a, b) => a || b);
 }
@@ -73,120 +81,130 @@ async function exists(pathname) {
 
 module.exports = {
   handler: init,
-  builder: _ => _
-    .option('dbengine', { alias: 'd', default: 'sqlite3' })
-    .default('dir', '.')
+  builder: _ =>
+    _.option('dbengine', { alias: 'd', default: 'sqlite3' }).default('dir', '.')
 };
 
 // TODO: generalize this function as ~ `generate(...)`
 function generateDatabaseConfig(database, dbengine) {
   switch (dbengine) {
-  case 'postgresql':
-    return {
-      development: {
-        client: 'pg',
-        host: 'localhost', port: 5432, database
-      },
-      test: {
-        client: 'pg',
-        host: 'localhost', 'port': 5432, database
-      },
-      production: {
-        client: 'pg',
-        host: 'localhost', port: 5432, database
-      }
-    };
-  default:
-    return {
-      development: {
-        client: 'sqlite3',
-        filename: './db/development.sqlite3'
-      },
-      test: {
-        client: 'sqlite3',
-        filename: './db/test.sqlite3'
-      },
-      production: {
-        client: 'sqlite3',
-        filename: './db/production.sqlite3'
-      },
-    };
+    case 'postgresql':
+      return {
+        development: {
+          client: 'pg',
+          host: 'localhost',
+          port: 5432,
+          database
+        },
+        test: {
+          client: 'pg',
+          host: 'localhost',
+          port: 5432,
+          database
+        },
+        production: {
+          client: 'pg',
+          host: 'localhost',
+          port: 5432,
+          database
+        }
+      };
+    default:
+      return {
+        development: {
+          client: 'sqlite3',
+          filename: './db/development.sqlite3'
+        },
+        test: {
+          client: 'sqlite3',
+          filename: './db/test.sqlite3'
+        },
+        production: {
+          client: 'sqlite3',
+          filename: './db/production.sqlite3'
+        }
+      };
   }
 }
 
 function generatePackageJSON(name, dbengine) {
   const dependencies = {
-    'apollo-cache-inmemory': '^1.2.1',
-    'apollo-client': '^2.3.1',
+    'apollo-cache-inmemory': '^1.3.0-beta.6',
+    'apollo-client': '^2.3.7',
     'apollo-link-http': '^1.5.4',
-    'axios': '^0.18.0',
-    'graphql': '^0.13.2',
+    axios: '^0.18.0',
+    graphql: '^0.13.2',
     'graphql-tag': '^2.9.2',
-    'huncwot': '^0.21.0',
-    'knex': '^0.14.6',
-    'sqlite3': '^4.0.0',
-    'validate': '^4.4.1',
-    'vue': '^2.5.16',
-    'vue-apollo': '^3.0.0-beta.10',
+    huncwot: '^0.22.0',
+    knex: '^0.15.2',
+    sqlite3: '^4.0.2',
+    validate: '^4.4.1',
+    vue: '^2.5.16',
+    'vue-apollo': '^3.0.0-beta.19',
     'vue-router': '^3.0.1',
-    'vuex': '^3.0.1'
+    vuex: '^3.0.1'
   };
 
   const devDependencies = {
-    '@types/node': '^10.0.8',
+    '@types/node': '^10.5.4',
     'babel-core': '^6.26.3',
-    'babel-loader': '^7.1.4',
+    'babel-loader': '^7.1.5',
     'babel-preset-env': '^1.7.0',
     'babel-preset-stage-3': '^6.24.1',
-    'concurrently': '^3.5.1',
-    'cross-env': '^5.1.5',
-    'css-loader': '^0.28.11',
+    concurrently: '^3.6.1',
+    'cross-env': '^5.2.0',
+    'css-loader': '^1.0.0',
     'file-loader': '^1.1.11',
     'friendly-errors-webpack-plugin': '^1.7.0',
     'html-webpack-plugin': '^3.2.0',
-    'node-sass': '^4.9.0',
-    'nodemon': '^1.17.4',
-    'sass-loader': '^7.0.1',
-    'typescript': '^2.8.3',
-    'vue-loader': '^15.0.10',
-    'vue-style-loader': '^4.1.0',
+    'node-sass': '^4.9.2',
+    nodemon: '^1.18.3',
+    'sass-loader': '^7.0.3',
+    typescript: '^2.9.2',
+    'vue-loader': '^15.2.6',
+    'vue-style-loader': '^4.1.1',
     'vue-template-compiler': '^2.5.16',
-    'webpack': '4.8.3',
-    'webpack-cli': '^2.1.3',
-    'webpack-dev-server': '^3.1.4',
-    'webpack-merge': '^4.1.2'
+    webpack: '4.16.3',
+    'webpack-cli': '^3.1.0',
+    'webpack-dev-server': '^3.1.5',
+    'webpack-merge': '^4.1.3'
   };
 
   switch (dbengine) {
-  case 'postgresql':
-    Object.assign(dependencies, { 'pg': 'latest' });
-    break;
-  default:
-    Object.assign(dependencies, { 'sqlite3': 'latest' });
-    break;
+    case 'postgresql':
+      Object.assign(dependencies, { pg: 'latest' });
+      break;
+    default:
+      Object.assign(dependencies, { sqlite3: 'latest' });
+      break;
   }
 
-  const browserslist = [
-    '> 1%',
-    'last 2 versions',
-    'not ie <= 8'
-  ];
+  const browserslist = ['> 1%', 'last 2 versions', 'not ie <= 8'];
 
   const scripts = {
-    'client': 'webpack-dev-server --mode development --open --config config/webpack.dev.js',
-    'build': 'webpack --progress --hide-modules --config config/webpack.prod.js',
-    'server': 'huncwot server',
+    client:
+      'webpack-dev-server --mode development --open --config config/webpack.dev.js',
+    build: 'webpack --progress --hide-modules --config config/webpack.prod.js',
+    server: 'huncwot server',
     'watch-ts': 'tsc -w',
-    'start': 'concurrently -k -p "[{name}]" -n "Client,TypeScript,Server" -c "yellow.bold,cyan.bold,green.bold" "yarn run client" "yarn run watch-ts" "yarn run server"'
+    start:
+      'concurrently -k -p "[{name}]" -n "Client,TypeScript,Server" -c "yellow.bold,cyan.bold,green.bold" "yarn run client" "yarn run watch-ts" "yarn run server"'
   };
 
-  return { name, version: '0.0.1', scripts, dependencies, devDependencies, browserslist };
+  return {
+    name,
+    version: '0.0.1',
+    scripts,
+    dependencies,
+    devDependencies,
+    browserslist
+  };
 }
 
 function generateSQL(name, dbengine) {
   switch (dbengine) {
-  case 'postgresql':
-    return `DROP DATABASE IF EXISTS ${name}_dev;
+    case 'postgresql':
+      return `DROP DATABASE IF EXISTS ${name}_dev;
 CREATE DATABASE ${name}_dev;
 
 \\c ${name}_dev;
@@ -203,8 +221,8 @@ VALUES
     ('Build a fantastic web application', false),
     ('Give back to the community', false);
 `;
-  case 'sqlite3':
-    return `CREATE TABLE tasks (
+    case 'sqlite3':
+      return `CREATE TABLE tasks (
   id INTEGER PRIMARY KEY,
   name VARCHAR,
   done INTEGER
