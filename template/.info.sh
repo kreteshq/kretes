c43c88c1d712f89ff5d4bb0e4fb9658c8ad78d4c
Zaiste <oh@zaiste.net>
2019-02-15 18:14:12 +0100

    feature: cli, add `info` page for guideance

---
+++ template/.info.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+#
+echo -e "\e[30;44m HUNCWOT \e[0m\e[30;107m 0.37.2 \e[0m\n\n"
+echo -e "\e[30;46m APP \e[0m \e[4mhttp://localhost:\e[36m8080\e[0m"
+echo -e "\e[30;45m API \e[0m \e[4mhttp://localhost:\e[35m5544\e[0m"
+echo -e "\n"
+echo -e "\e[30;107m NEXT STEPS \e[0m\n"
+echo -e "  - create a request handler using \e[33mhuncwot create handler <name>\e[0m"
+echo -e "    learn more at: https://huncwot.org/concept/handler"
+echo -e "  - create a RESTful resource using \e[33mhuncwot create resource <name>\e[0m"
+read
