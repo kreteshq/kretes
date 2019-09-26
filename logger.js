317f1cd4e28d3df12cac4ba127f1e5cfaa51b33d
Zaiste <oh@zaiste.net>
2019-09-26 18:56:43 +0200

    feature/cli: display params content on CLI

--- logger.js
+++ logger.js
@@ -1,3 +1,4 @@
+const util = require('util');
 const color = require('chalk');
 const { parse } = require('url');
 const stackParser = require('error-stack-parser');
@@ -6,20 +7,24 @@ const explain = require('./explainer');
 
 class Logger {
   static printRequestResponse(context) {
-    const { request, response } = context;
+    const { request, response, params } = context;
     const { method } = request;
     const { pathname, _query } = parse(context.request.url, true); // TODO Test perf vs RegEx
     const { statusCode } = response;
-    const statusMessage = '';
+    const statusMessage = 'Internal Server Error';
 
     console.log(
-      color`{green ●} {magenta ${method}} {bold ${pathname}} -> {red ${statusCode}} ${statusMessage}`
+      color`{green ●} {magenta ${method}} {bold ${pathname}} → {red ${statusCode}} ${statusMessage}
+  {gray Params}
+${util
+  .inspect(params, { compact: false, colors: true, sorted: true })
+  .slice(2, -2)}`
     );
   }
 
   static printError(error, layer = 'General') {
     console.error(
-      color`{red ●} {bold.red Error}{gray /}${layer}: {bold ${error.message}}
+      color`  {bold.red Error} {bold.underline ${error.message}}
   {gray Explanation} \n  ${explain.for(error)}
 \n  {gray Stack trace}`
     );
