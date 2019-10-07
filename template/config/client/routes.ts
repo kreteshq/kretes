9a929dab8e46edf8d49a3af474a7b3a6042bb611
Zaiste <oh@zaiste.net>
2019-10-07 20:21:17 +0200

    fix: make Webpack compile from template for client

--- template/config/client/routes.ts
+++ template/config/client/routes.ts
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 
-import Home from '~/base/component/Home.vue';
+import Home from '../../features/base/component/Home.vue';
 
 Vue.use(Router);
 
