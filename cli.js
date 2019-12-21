8a5f245bf03cc920aa8f84fa13c6eda69f8d99fb
Zaiste <oh@zaiste.net>
2019-12-21 22:28:57 +0100

    fix: make prettier happy

--- cli.js
+++ cli.js
@@ -14,7 +14,9 @@
 // limitations under the License.
 
 const { valid, satisfies, validRange } = require('semver');
-const { engines: { node: version } } = require('./package.json');
+const {
+  engines: { node: version }
+} = require('./package.json');
 
 const expected = validRange(version);
 const actual = valid(process.version);
@@ -27,67 +29,25 @@ if (!satisfies(actual, expected)) {
 const _argv = require('yargs')
   .version()
   .usage('Usage: $0 <command> [options]')
-  .command(
-    ['new [dir]', 'init', 'n'],
-    'Create new project',
-    require('./cli/init')
-  )
+  .command(['new [dir]', 'init', 'n'], 'Create new project', require('./cli/init'))
   .example('$0 new my-project', 'Create and initialize `my-project` directory')
   .command(['setup [name]'], 'Setup an integration', require('./cli/setup'))
-  .command(
-    ['start', 'start', 's'],
-    'Start the application',
-    require('./cli/start')
-  )
+  .command(['start', 'start', 's'], 'Start the application', require('./cli/start'))
   .example('$0 start', 'Start the application')
-  .command(
-    ['client', 'client', 'c'],
-    'Start only the client',
-    require('./cli/client')
-  )
+  .command(['client', 'client', 'c'], 'Start only the client', require('./cli/client'))
   .example('$0 client', 'Start only the client')
-  .command(
-    ['server [dir]', 'serve', 'se'],
-    'Serve the directory',
-    require('./cli/server')
-  )
+  .command(['server [dir]', 'serve', 'se'], 'Serve the directory', require('./cli/server'))
   .example('$0 server --port 4000', 'Serve the directory at the port 4000')
-  .command(
-    ['build', 'build', 'b'],
-    'Build the application for production',
-    require('./cli/build')
-  )
+  .command(['build', 'build', 'b'], 'Build the application for production', require('./cli/build'))
   .example('$0 build', 'Build the application for production')
-  .command(
-    ['database [command]', 'db'],
-    'Database operations',
-    require('./cli/database')
-  )
-  .command(
-    ['deploy', 'deploy', 'de'],
-    'Deploy the application',
-    require('./cli/deploy')
-  )
+  .command(['database [command]', 'db'], 'Database operations', require('./cli/database'))
+  .command(['deploy', 'deploy', 'de'], 'Deploy the application', require('./cli/deploy'))
   .example('$0 deploy', 'Deploy the application')
-  .command(
-    ['generate [command]', 'g'],
-    'Generate various artifacts',
-    require('./cli/generate')
-  )
+  .command(['generate [command]', 'g'], 'Generate various artifacts', require('./cli/generate'))
   .command(['routes', 'r'], 'Display routes', require('./cli/routes'))
-  .command(
-    ['migrate', 'm'],
-    'Run database migrations',
-    require('./cli/migrate')
-  )
-  .command(
-    ['background', 'bg'],
-    'Run background processing',
-    require('./cli/background')
-  )
+  .command(['migrate', 'm'], 'Run database migrations', require('./cli/migrate'))
+  .command(['background', 'bg'], 'Run background processing', require('./cli/background'))
   .demandCommand(1, 'You need at least one command before moving on')
   .help('h')
   .alias('h', 'help')
-  .epilogue(
-    'for more information, find the documentation at https://huncwot.org'
-  ).argv;
+  .epilogue('for more information, find the documentation at https://huncwot.org').argv;
