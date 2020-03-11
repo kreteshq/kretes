9c577d1d93afe0bab1deac6c0c52649a61cc40e2
Emil Ajdyna <emil@ajdyna.com>
2020-03-11 21:33:58 +0100

    Simplify the prepack script to utilize JSON.stringify formatting

--- scripts/prepack.js
+++ scripts/prepack.js
@@ -1,24 +1,10 @@
-const { writeFileSync, readFileSync } = require('fs');
-const { join } = require('path');
+const { writeFileSync } = require('fs');
+const { join } = require('path')
+const { version } = require('../package.json');
 
-const prettier = require('prettier');
+const packagePath = join(__dirname, '../template/base/package.json')
+const pkg = require(packagePath);
 
-const mainPackageJson = require('../package.json');
-
-/**
- * @param {string} templatePath
- */
-function setVersionInTemplate(templatePath) {
-  const packagePath = join(templatePath, 'package.json');
-
-  const pkg = JSON.parse(readFileSync(packagePath, { encoding: 'utf-8' }));
-  pkg.dependencies.huncwot = `^${mainPackageJson.version}`;
-  const text = prettier.format(JSON.stringify(pkg), {
-    filepath: packagePath,
-    ...mainPackageJson.prettier
-  });
-
-  writeFileSync(packagePath, text, { encoding: 'utf-8' });
-}
-
-setVersionInTemplate(join(__dirname, '../template/base'));
+pkg.dependencies.huncwot = `^${version}`;
+const content = JSON.stringify(pkg, null, 2) + "\n";
+writeFileSync(packagePath, content, { encoding: 'utf-8' });
