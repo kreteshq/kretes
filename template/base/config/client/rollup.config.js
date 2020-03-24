a81d1e7afd27f6be5b8e8b4da67e19083ccf1663
Zaiste <oh@zaiste.net>
2020-03-24 14:43:32 +0100

    refactor/cli: simplify snowpack modules refs

--- template/base/config/client/rollup.config.js
+++ template/base/config/client/rollup.config.js
@@ -2,15 +2,6 @@ import ts from '@wessberg/rollup-plugin-ts';
 import serve from 'rollup-plugin-serve';
 import livereload from 'rollup-plugin-livereload';
 
-const modules = () => ({
-  resolveId(importee) {
-    if (importee.startsWith('@')) {
-      return { id: importee.replace('@', '.'), external: true };
-    }
-    return null;
-  }
-});
-
 export default {
   input: 'config/client/index.ts',
   output: {
@@ -22,7 +13,6 @@ export default {
     exclude: 'node_modules/**'
   },
   plugins: [
-    modules(),
     ts({ tsconfig: 'config/client/tsconfig.json' }),
     serve('public'),
     livereload('public')
