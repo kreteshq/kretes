c3b411793f9c58b3ab9bfa9097e34510e4e5f9cd
Zaiste <oh@zaiste.net>
2020-04-06 18:10:04 +0200

    refactor/client: adjust snowpack config

--- template/base/config/client/rollup.config.js
+++ template/base/config/client/rollup.config.js
@@ -1,16 +1,26 @@
 import ts from '@wessberg/rollup-plugin-ts';
+import { snowpackImportRewriter } from 'huncwot/compiler/typescript';
 
 export default {
   input: 'config/client/index.ts',
   output: {
     dir: 'public',
-    sourcemap: true
+    sourcemap: true,
+    paths: {
+      tslib: '/modules/tslib.js'
+    }
   },
+  external: id => id.startsWith('/modules') || id == 'tslib',
   watch: {
     chokidar: true,
     exclude: 'node_modules/**'
   },
   plugins: [
-    ts({ tsconfig: 'config/client/tsconfig.json' })
+    ts({
+      tsconfig: 'config/client/tsconfig.json',
+      transformers: {
+        after: [snowpackImportRewriter()]
+      }
+    }),
   ]
 };
