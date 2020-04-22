d5bcf54079ff501d1e0436b8ffe2c91d4b9e211c
Zaiste <oh@zaiste.net>
2020-04-22 13:21:24 +0200

    feature/client: display compilation info

--- cli/client.js
+++ cli/client.js
@@ -1,15 +1,5 @@
-// Copyright 2019 Zaiste & contributors. All rights reserved.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// Copyright Zaiste. All rights reserved.
+// Licensed under the Apache License, Version 2.0
 
 const debug = require('debug')('server'); // eslint-disable-line no-unused-vars
 const { spawn } = require('child_process');
@@ -18,10 +8,16 @@ const fs = require('fs-extra');
 const rollup = require('rollup');
 const loadConfigFile = require('rollup/dist/loadConfigFile');
 const WebSocket = require('ws');
+const color = require('chalk');
 
 const cwd = process.cwd();
+const VERSION = require('../package.json').version;
 
 const client = async () => {
+  console.log(
+    color`{bold.blue ┌ Kretes} {bold ${VERSION}} \n{bold.blue └ }{blue Client-Side Compilation}\n  {grey Hot Reload} {green enabled}`
+  );
+
   await fs.ensureDir(join(cwd, 'modules'));
 
   const template = join(cwd, 'config/client', 'index.html');
@@ -68,7 +64,7 @@ const client = async () => {
 
   loadConfigFile(join(cwd, 'config/client/rollup.config.js'), { format: 'esm' })
     .then(async ({options, warnings}) => {
-      console.log(`${warnings.count} warnings`);
+      console.log(`  ${warnings.count} warnings`);
 
       warnings.flush();
 
@@ -80,10 +76,11 @@ const client = async () => {
       wss.on('connection', function connection(ws) {
         watcher.on('event', event => {
           if (event.code == 'END') ws.send('reload');
+          if (event.code == 'ERROR') console.error(event.frame);
         });
       });
 
-      console.log('Client started for http://localhost:5544');
+      console.log(color`  {bold.underline http://localhost:5544}`);
     });
 };
 
