ff5db5a48b1962ac8b76f21fce226a46073b0397
Zaiste <oh@zaiste.net>
2020-05-13 15:48:26 +0200

    refactor/background: adapt to TypeScript

--- source/background.ts
+++ source/background.ts
@@ -1,11 +1,10 @@
 // Copyright Zaiste. All rights reserved.
 // Licensed under the Apache License, Version 2.0
 
-const db = require('./db.js');
+import db from './db.js';
 
-module.exports = {
-  async schedule({ task, payload = {}, queue = null, runAt = null, maxAttempts = null }) {
-    await db.sql`
+const schedule = async ({ task, payload = {}, queue = null, runAt = null, maxAttempts = null }) => {
+  await db.sql`
     SELECT * FROM graphile_worker.add_job(
       identifier => ${task.name}::text,
       payload => ${payload}::json,
@@ -13,5 +12,8 @@ module.exports = {
       run_at => coalesce(${runAt ? runAt.toISOString() : null}::timestamptz, now()),
       max_attempts => coalesce(${maxAttempts}::int, 25)
     );`;
-  }
+}
+
+export {
+  schedule,
 };
