9e0facd82d0d9e22849904732ca3738e8829ba9f
Zaiste <oh@zaiste.net>
2020-07-11 15:45:34 +0200

    re/tmpl: remove loader config (outdated)

--- template/base/config/loaders/docs.js
+++
@@ -1,9 +0,0 @@
-module.exports = function(source, map) {
-  this.callback(
-    null,
-    `module.exports = function(Component) {
-      Component.options.__docs = ${JSON.stringify(source)}
-    }`,
-    map
-  );
-};
