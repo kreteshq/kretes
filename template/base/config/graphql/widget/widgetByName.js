21e5eb27096873e81bf41f2416b6075428fe542b
Zaiste <oh@zaiste.net>
2020-07-11 15:44:57 +0200

    re/tmpl: remove GraphQL config (outdated)

--- template/base/config/graphql/widget/widgetByName.js
+++
@@ -1,8 +0,0 @@
-function widgetByName(_, { name }) {
-  return {
-    id: 1,
-    name: 'Widget 1'
-  };
-}
-
-module.exports = widgetByName;
--- template/base/config/graphql/widget/widgets.js
+++
@@ -1,8 +0,0 @@
-function widgets() {
-  return [
-    { id: 1, name: 'Widget 11' },
-    { id: 2, name: 'Widget 22' }
-  ];
-}
-
-module.exports = widgets;
