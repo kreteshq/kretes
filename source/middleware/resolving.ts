0ebce25ff849feb11d767b1d94b38c2b53571c87
Zaiste <oh@zaiste.net>
2020-08-08 16:42:41 +0200

    fe: custom resolver for `node_modules`

--- source/middleware/resolving.ts
+++ source/middleware/resolving.ts
@@ -3,12 +3,26 @@
 import Debug from "debug";
 const debug = Debug('ks:middleware:resolving') // eslint-disable-line no-unused-vars
 
+const Module = require('module');
 import fs from 'fs-extra';
 import { join } from 'path';
 
 import RE from '../regexp';
 import { JavaScriptString, InternalServerError } from '../response';
 import { Vue } from '../manifest';
+
+const resolve = (directory, moduleName) => {
+  const filename = join(directory, 'noop.js');
+
+  const paths = Module._nodeModulePaths(directory);
+  const makeResolver = () => Module._resolveFilename(moduleName, {
+    id: filename,
+    filename,
+    paths
+  });
+
+  return makeResolver();
+}
 
 const Resolving = () => {
   return async ({ path }: any, next: any) => {
@@ -28,8 +42,14 @@ const Resolving = () => {
       const packageJSONPath = join(process.cwd(), 'node_modules', id, 'package.json');
       const packageJSONContent = require(packageJSONPath);
 
+      let modulePath;
       // get the ES module path by hand FIXME
-      const modulePath = join(process.cwd(), 'node_modules', id, packageJSONContent.module);
+      if (packageJSONContent.module) {
+        modulePath = join(process.cwd(), 'node_modules', id, packageJSONContent.module);
+      } else {
+        modulePath = resolve(process.cwd(), id);
+      }
+
       const content = await fs.readFile(modulePath, 'utf-8')
       return JavaScriptString(content);
     } catch (error) {
