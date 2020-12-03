085442b66e462223985d5ee5df25b75de495140c
Zaiste <oh@zaiste.net>
2020-12-03 17:13:52 +0100

    fe: integrate `react-query`, React template

--- template/react/features/Base/View/index.tsx
+++ template/react/features/Base/View/index.tsx
@@ -1,14 +1,30 @@
 import React, { useState } from 'react';
+import { QueryCache, ReactQueryCacheProvider, useQuery } from 'react-query';
+
+const queryCache = new QueryCache()
+
+const request = () => fetch('https://freegeoip.app/json/').then(response => response.json());
 
 function App() {
   const [name, setName] = useState('Kretes');
+  const { data, isLoading, error } = useQuery('ip', request);
+
+  if (isLoading) return 'Loading...'
+  if (error) return 'Error: ' + error.message;
+
+  const { country_name, city, ip } = data;
 
   return (
-    <div className="max-w-2xl mx-auto">
-      <div className="p-4 bg-white shadow">
-        Hello, <span className="font-semibold">{name}</span>
+    <ReactQueryCacheProvider queryCache={queryCache}>
+      <div className="max-w-2xl mx-auto">
+        <div className="text-xl">
+          Hello, <span className="font-semibold">{name}</span>
+        </div>
+        <div className="text-gray-400">
+          You are now in {country_name}, {city} and your IP is {ip}
+        </div>
       </div>
-    </div>
+    </ReactQueryCacheProvider>
   );
 }
 
