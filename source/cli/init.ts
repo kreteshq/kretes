7dcbbaee27eed8ee65c8bce020181c16c6f8854c
Zaiste <oh@zaiste.net>
2020-12-03 17:17:36 +0100

    fix: issues with the `locale` command

--- source/cli/init.ts
+++ source/cli/init.ts
@@ -73,7 +73,8 @@ export async function handler({ dir, installDependencies, template }) {
     // Setup development database
     const stdout = fs.openSync(join(cwd, dir, 'log/database.log'), 'a');
 
-    await run('/usr/bin/env', ['nix-shell', '--pure', '--run', 'initdb -A trust --encoding=UTF8 --locale=en_US.UTF-8 --lc-collate=C'], { stdout, cwd: projectDir  });
+    // FIXME investigate: no `pure` as `locale` error on MacOS
+    await run('/usr/bin/env', ['nix-shell', '--run', 'initdb -A trust --encoding=UTF8 --locale=en_US.UTF-8 --lc-collate=C'], { stdout, cwd: projectDir  });
     await run('/usr/bin/env', ['nix-shell', '--pure', '--run', 'pg_ctl start -o "-k /tmp" -l ./log/postgresql.log'], { stdout, cwd: projectDir });
     await run('/usr/bin/env', ['nix-shell', '--pure', '--run', `createdb ${name}`], { cwd: projectDir });
     await run('/usr/bin/env', ['nix-shell', '--pure', '--run', 'pg_ctl stop -o "-k /tmp" -l ./log/postgresql.log'], { stdout, cwd: projectDir });
