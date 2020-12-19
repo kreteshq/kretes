16bf352e20acd8032d1d54d36c4b9ae81a16e5f9
Zaiste <oh@zaiste.net>
2020-12-19 16:06:37 +0100

    re: create database with name from config

--- source/cli/database/create.ts
+++ source/cli/database/create.ts
@@ -4,15 +4,28 @@
 const { exec } = require('child_process');
 const { basename } = require('path');
 const cwd = process.cwd();
+import * as _ from 'colorette';
 
-export const handler = () => {
-  let app = basename(cwd);
-  app = app.split('-').join('_');
+import { print, println } from '../../util';
 
-  console.log(`Creating database... ${app}`);
-  const { stdout, stderr } = exec(`createdb ${app}`, { cwd });
+export const handler = async () => {
+  const { default: config } = await import('config'); // defer the config loading
+
+  if (!config.has("db")) {
+    println(`${_.red('Error')}`)
+    println(`Provide the database details in 'config/default.json'`)
+
+    process.exit(1);
+  }
+
+  const { database } = config.get('db');
+
+  print(`Creating database '${database}': `);
+  const { stdout, stderr } = exec(`createdb ${database}`, { cwd });
   stdout.pipe(process.stdout);
   stderr.pipe(process.stderr);
+
+  println(`${_.green('OK')}\n`);
 }
 
 export const command = 'create';
