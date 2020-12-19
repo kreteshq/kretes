e16c0eb2adc6d96a5fdf88e63a801ce56fd44f93
Zaiste <oh@zaiste.net>
2020-12-19 19:35:06 +0100

    fe: setup db from default location

--- source/cli/database/setup.ts
+++ source/cli/database/setup.ts
@@ -8,7 +8,7 @@ import * as _ from 'colorette';
 import { run, print, println } from '../../util';
 
 export const handler = async () => {
-  print('Setting up the database: ');
+  print('Setting up the database content: ');
   const { default: config } = await import('config'); // defer the config loading
 
   const CWD = process.cwd();
@@ -20,26 +20,16 @@ export const handler = async () => {
     process.exit(1);
   }
 
-  const { database, username } = config.get('db');
+  const { database, user } = config.get('db');
 
-  // Setup development database
+  const stdin = FS.openSync(Path.join(CWD, 'db/setup.sql'), 'r');
   const stdout = FS.openSync(Path.join(CWD, 'log/database.log'), 'a');
 
-  // FIXME investigate: no `pure` as `locale` error on MacOS
   try {
-    await run('/usr/bin/env', ['nix-shell', '--run', 'initdb -A trust --encoding=UTF8 --locale=en_US.UTF-8 --lc-collate=C'], { stdout });
+    await run('/usr/bin/env', ['psql', database, user], { stdout, stdin });
   } catch (error) {
     println(error.message);
   }
-  await run('/usr/bin/env', ['nix-shell', '--pure', '--run', 'pg_ctl start -o "-k /tmp" -l ./log/postgresql.log'], { stdout });
-  await run('/usr/bin/env', ['nix-shell', '--pure', '--run', `createdb ${database}`]);
-  await run('/usr/bin/env', ['nix-shell', '--pure', '--run', 'pg_ctl stop -o "-k /tmp" -l ./log/postgresql.log'], { stdout });
-
-  // const psql = `psql ${database} ${username} < db/setup.sql`;
-  // const { stdout, stderr } = exec(psql, { cwd });
-
-  // stdout.pipe(process.stdout);
-  // stderr.pipe(process.stderr);
 
   println(` ${_.green('OK')}\n`);
 }
