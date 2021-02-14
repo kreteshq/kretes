a2fdcf3c4f2deb721b1c44a7c20131912ac589cb
Zaiste <oh@zaiste.net>
2021-02-14 13:48:21 +0100

    fix: @/components setup for the React.js template

--- template/react/site/index.tsx
+++ template/react/site/index.tsx
@@ -4,7 +4,7 @@ import { QueryClient, QueryClientProvider } from 'react-query';
 
 const queryClient = new QueryClient()
 
-import { App } from '@/App';
+import { App } from '@/components';
 
 render(
   <QueryClientProvider client={queryClient}>
