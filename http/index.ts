8e42f1426533c439a0bbea43cc684d7db6aac9c8
Zaiste <oh@zaiste.net>
2021-02-18 12:01:10 +0100

    re: better support for subpaths in `http`

--- http/index.ts
+++ http/index.ts
@@ -13,10 +13,20 @@ const defaultHeaders = {
   'Content-Type': 'application/json'
 };
 
-export function POST(path: string, payload: Payload, headers = defaultHeaders) {
-  return fetch(path, {
-    method: 'POST',
-    headers,
-    body: JSON.stringify(payload)
-  })
-}
+const params = (method: string, payload: Payload, headers) => ({
+  method,
+  headers,
+  body: JSON.stringify(payload)
+})
+
+export const POST = (path: string, payload: Payload, headers = defaultHeaders) =>
+  fetch(path, params('POST', payload, headers))
+
+export const PUT = (path: string, payload: Payload, headers = defaultHeaders) =>
+  fetch(path, params('PUT', payload, headers))
+
+export const PATCH = (path: string, payload: Payload, headers = defaultHeaders) =>
+  fetch(path, params('PATCH', payload, headers))
+
+export const DELETE = (path: string, headers = defaultHeaders) =>
+  fetch(path, { method: 'DELETE', headers })
