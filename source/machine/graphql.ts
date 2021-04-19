70691679982c8496d9508c3b08b6a42e0ea525ae
Zaiste <oh@zaiste.net>
2021-04-19 11:39:28 +0200

    fe: initial GraphQL extension (WIP)

--- source/machine/graphql.ts
+++ source/machine/graphql.ts
@@ -1,9 +1,31 @@
 import { graphql } from "graphql";
 import { withPostGraphileContext, createPostGraphileSchema, } from "postgraphile";
 import PgSimplifyInflectorPlugin from "@graphile-contrib/pg-simplify-inflector";
+import { makeExtendSchemaPlugin, gql } from "graphile-utils";
 
 async function makeGraphQLRunner(pgPool) {
-  const schema = await createPostGraphileSchema(pgPool, "public", { appendPlugins: [PgSimplifyInflectorPlugin] });
+  const schema = await createPostGraphileSchema(pgPool, "public", { 
+    appendPlugins: [
+      PgSimplifyInflectorPlugin,
+      makeExtendSchemaPlugin(build => {
+        // Get any helpers we need from `build`
+        const { pgSql: sql, inflection } = build;
+      
+        return {
+          typeDefs: gql`
+            extend type Query {
+              hello: String
+            }
+          `,
+          resolvers: {
+            Query: {
+              hello: () => "Hello, GraphQL"
+            }
+          },
+        };
+      }),
+    ] 
+  });
 
   const query = async (graphqlQuery, variables = {}) => {
     return await withPostGraphileContext({ pgPool },
