6b133d34e63c0a3ddd863b13d68fa6662407f052
Zaiste <oh@zaiste.net>
2021-04-28 12:47:20 +0200

    re: improve the React.js template

--- template/react/components/App.tsx
+++ template/react/components/App.tsx
@@ -10,6 +10,7 @@ const request = () => fetch('/_api/base').then(response => response.json());
 
 export const App: React.FC = () => {
   const [name] = useState('Kretes');
+  const [counter, setCounter] = useState(1);
   const { data, isLoading, error } = useQuery<City, Error>('example', request);
 
   if (isLoading) return <div>Loading...</div>;
@@ -21,13 +22,21 @@ export const App: React.FC = () => {
     <main>
       <section>
         <header>
+          <img src="/huncwot.svg" width="120" />
           <h1>
             Hello, <span className="font-semibold">{name}</span>
           </h1>
           <div>
             You are now in {city} and your IP is {ip}
           </div>
-          <small>(refresh this page)</small>
+          <small>(refresh the page)</small>
+          <hr/>
+          <h2>{counter}</h2>
+          <p>
+            <a href="#" onClick={() => setCounter(counter - 1)}><b>Dec</b></a>
+            <a href="#" onClick={() => setCounter(counter + 1)}><b>Inc</b></a>
+          </p>
+          <small>(play with the counter)</small>
         </header>
       </section>
     </main>
