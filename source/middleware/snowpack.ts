c12cdbf58b014bbfd61e8df58c97c8877e4510ff
Zaiste <oh@zaiste.net>
2021-07-14 19:51:50 +0200

    fix: handle the not found asset

--- source/middleware/snowpack.ts
+++ source/middleware/snowpack.ts
@@ -1,19 +1,23 @@
 // Copyright Zaiste. All rights reserved.
 // Licensed under the Apache License, Version 2.0
-import { Middleware } from "retes";
-import { SnowpackDevServer } from "snowpack";
+import { Middleware } from 'retes';
+import { SnowpackDevServer } from 'snowpack';
 
-import { OK } from '../response';
+import { NotFound, OK } from '../response';
 
 export const Snowpack = (server: SnowpackDevServer): Middleware => {
-  return next => async request => {
+  return (next) => async (request) => {
     const { url } = request;
 
-    if (url.endsWith(".js") || url.endsWith(".css")) {
-      const { contents, contentType } = await server.loadUrl(url);
-      return OK(contents.toString(), { 'Content-Type': contentType })
+    if (url.endsWith('.js') || url.endsWith('.css')) {
+      try {
+        const { contents, contentType } = await server.loadUrl(url);
+        return OK(contents.toString(), { 'Content-Type': contentType });
+      } catch (error) {
+        return NotFound();
+      }
     }
 
-    return next(request)
+    return next(request);
   };
 };
