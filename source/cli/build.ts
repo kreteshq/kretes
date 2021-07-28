7ec549ead1f16dda4f5052059d1acc0d4f812528
Zaiste <oh@zaiste.net>
2021-07-28 13:19:06 +0200

    fix: server location for `tsconfig.json`

--- source/cli/build.ts
+++ source/cli/build.ts
@@ -19,16 +19,16 @@ import { compileCSS } from '../compiler/css';
 
 const compileServerSide = () => {
   const TS = require('typescript/lib/typescript');
-  const compiler = new TypescriptCompiler(process.cwd(), 'config/server/tsconfig.json', TS);
-  const { error, config } = compiler.configParser().parse()
+  const compiler = new TypescriptCompiler(process.cwd(), 'server/tsconfig.json', TS);
+  const { error, config } = compiler.configParser().parse();
   if (error || !config) {
-    console.log(error)
-    return
+    console.log(error);
+    return;
   }
 
   if (config.errors.length) {
-    console.log(config.errors)
-    return
+    console.log(config.errors);
+    return;
   }
 
   //@ts-ignore
@@ -37,40 +37,37 @@ const compileServerSide = () => {
     const host = ts.createCompilerHost(options);
     const program = ts.createProgram(fileNames, options, host);
     const r = transformPaths(program);
-    return context => r.before(context);
+    return (context) => r.before(context);
   };
 
   compiler.use(plugin, 'before');
 
-  const { diagnostics } = compiler.builder(config!).build()
+  const { diagnostics } = compiler.builder(config!).build();
 
   return diagnostics;
-}
+};
 
 export const handler = async () => {
   const cwd = process.cwd();
 
-  process.env.KRETES = process.env.NODE_ENV = 'production'; 
+  process.env.KRETES = process.env.NODE_ENV = 'production';
 
   try {
     println(notice('Build'));
 
     const { parsed: envs = {} } = dotenv.config();
-    const env = Object.fromEntries(Object.entries(envs)
-      .filter(([name, value]) => name.startsWith("PUBLIC_")));
+    const env = Object.fromEntries(Object.entries(envs).filter(([name, value]) => name.startsWith('PUBLIC_')));
 
     const config = createConfiguration({
       ...DefaultSnowpackConfig,
-      env, 
-      plugins: [
-        ["@snowpack/plugin-postcss", { config: join(process.cwd(), 'config', 'postcss.config.js') }],
-      ]
-    })
+      env,
+      plugins: [['@snowpack/plugin-postcss', { config: join(process.cwd(), 'config', 'postcss.config.js') }]],
+    });
 
     const result = await build({ config, lockfile: null });
 
     const indexHTML = join(cwd, 'public', 'index.html');
-    let html = await fs.readFile(indexHTML, 'utf-8')
+    let html = await fs.readFile(indexHTML, 'utf-8');
     // html = html!.replace('</body>', `${scriptSnippet}\n</body>`)
     await fs.outputFile(indexHTML, html);
 
@@ -78,12 +75,12 @@ export const handler = async () => {
 
     const diagnostics = compileServerSide();
 
-    print(`${__.green('done')}\n`)
+    print(`${__.green('done')}\n`);
 
-    process.exit(0) // FIXME bug in Snowpack API, it doesn't stop
+    process.exit(0); // FIXME bug in Snowpack API, it doesn't stop
   } catch (error) {
     console.error(__`  {red Error}: ${error.message}`);
   }
 };
 
-export const builder = _ => _;
+export const builder = (_) => _;
